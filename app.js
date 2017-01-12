0742384ad646d54fc2882e3439a61ff97801b175
Sonny-cole Naomie <sonny-cole@MC-G-LP-C120-1.local>
2017-01-12 15:40:42 +0100

    Correction

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var songs = require('./routes/songs');
-var artist = require('./routes/artist');
+
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/songs', songs);
-app.use('/artist', artist);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
